8e106a53f96bb07737c1c3038e46d831200cb0a0
aspcodenet <stefan.holmberg@systementor.se>
2023-04-26 14:57:20 +0200

    a

--- main.c
+++ main.c
@@ -10,45 +10,31 @@
 // B (digital pin 8 to 13)
 // C (analog input pins)
 // D (digital pins 0 to 7)
+// https://wokwi.com/projects/363070419462614017
 
+#define RED 2
+#define YELLOW 1
+#define GREEN 0
 
-//if (PIND & (1<<PD2)) 
-//Blinka #13 = interna lampan på Uno
-//Dvs pin 5 på port B
-//https://www.arduino.cc/en/Reference/PortManipulation
-//
-void internalLed()
-{
-	DDRB |= (1 << 0) | (1 << 1) | (1 << 2);
-	BIT_CLEAR(DDRD, 6);
-	BIT_SET(PORTD, 6); //This means INPUT_PULLUP
-	// https://forum.arduino.cc/t/using-avr-internal-pull-up-for-push-button-controlling/327729/4
-	// https://www.hackster.io/Hack-star-Arduino/push-buttons-and-arduino-a-simple-guide-wokwi-simulator-c2281f
-
-	while (1)
-	{
-		if(BIT_CHECK(PIND,6))
-		{
-			BIT_SET(PORTB, 2);
-			BIT_CLEAR(PORTB, 1);
-			BIT_CLEAR(PORTB, 0);
-		}
-		else {
-		BIT_CLEAR(PORTB, 2);
-		BIT_SET(PORTB, 1);
-		BIT_CLEAR(PORTB, 0);
-
-		}
-		_delay_ms(100);
-	}
-
-}
-
-
-
-//Blinka pin 7 (PORT D)
 int main(void)
 {
-	internalLed();
+    BIT_SET(DDRB,RED);
+    BIT_SET(DDRB,YELLOW);
+    BIT_SET(DDRB,GREEN);
+    while(1) {
+        BIT_SET(PORTB, RED);
+        BIT_CLEAR(PORTB, YELLOW);
+        BIT_CLEAR(PORTB, GREEN);
+		_delay_ms(15000);
+        BIT_CLEAR(PORTB, RED);
+        BIT_SET(PORTB, YELLOW);
+        BIT_CLEAR(PORTB, GREEN);
+		_delay_ms(3000);
+        BIT_CLEAR(PORTB, RED);
+        BIT_CLEAR(PORTB, YELLOW);
+        BIT_SET(PORTB, GREEN);
+		_delay_ms(15000);
+
+    }
 	return 0;
 }
